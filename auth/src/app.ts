10daf40bc12f28ed73ea02a00b285d28c6464568
borosakos <boros.aki@gmail.com>
2024-02-24 20:37:56 +0100

    feat: Add routes to auth

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,12 +1,18 @@
 import express from "express";
 
+import { currentUserRouter } from "./routes/currentUser";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
 app.use(express.json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Welcome!");
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 export default app;
