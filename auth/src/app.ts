0c0748fc067d89ae9ceebadcebe0cf54697332fe
borosakos <boros.aki@gmail.com>
2024-02-24 23:28:56 +0100

    feat: Add error handling middleware

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -4,6 +4,7 @@ import { currentUserRouter } from "./routes/currentUser";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
@@ -14,5 +15,7 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.use(errorHandler);
 
 export default app;
