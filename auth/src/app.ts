0e4fe770852cffe5a1d083f4b43e5d1e03d12700
borosakos <boros.aki@gmail.com>
2024-02-25 10:39:01 +0100

    feat: Add NotFoundError with async error handling

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import "express-async-errors";
 
 import { currentUserRouter } from "./routes/currentUser";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/errorHandler";
+import { NotFoundError } from "./errors/notFoundError";
 
 const app = express();
 
@@ -15,6 +17,8 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all("*", async () => { throw new NotFoundError(); });
 
 app.use(errorHandler);
 
