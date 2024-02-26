e8fb8a06868f701c5df1eff0a3eb8283cdd2db3b
borosakos <boros.aki@gmail.com>
2024-02-26 23:31:48 +0100

    refactor: Create validateRequest middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,24 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+
+import { validateRequest } from "../middlewares/validateRequest";
 
 const router = express.Router()
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Sign in!");
-});
+router.post("/api/users/signin", [
+  body("email")
+    .isEmail()
+    .withMessage("Please, provide a valid email address!"),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Please, provide your password!")
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => { 
+    const { email, password } = req.body;
+    res.send(200);
+  }
+);
 
 export { router as signinRouter }
