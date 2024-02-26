17d174be2f848fdcdc8d2e53a271442bf781ab6c
borosakos <boros.aki@gmail.com>
2024-02-26 21:10:52 +0100

    feat: Add session cookie handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/currentUser";
 import { signinRouter } from "./routes/signin";
@@ -10,9 +11,15 @@ import { errorHandler } from "./middlewares/errorHandler";
 import { NotFoundError } from "./errors/notFoundError";
 
 const app = express();
-
+app.set("trust proxy", true);
 app.set("port", process.env.PORT || 3000);
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
