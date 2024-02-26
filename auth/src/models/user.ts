ac9bc4d187b6be7d1555ecc5bfbbac6603161bb2
borosakos <boros.aki@gmail.com>
2024-02-26 22:29:28 +0100

    feat: Remove _id and __v from User response

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -24,6 +24,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
+}, {
+  toJSON: {
+    versionKey: false,
+    transform(doc, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.password;
+    }
+  }
 });
 
 userSchema.pre("save", async function (done) {
