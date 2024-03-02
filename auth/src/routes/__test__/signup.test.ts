3257dc725d1b46e8c697eb386cc9bc786af4b931
borosakos <boros.aki@gmail.com>
2024-03-02 19:56:19 +0100

    test: Add Set-Cookie check to signup

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -64,3 +64,15 @@ it("disallows duplicate emails", async () => {
     })
     .expect(400);
 });
+
+it("sets a cookie after succesfull signup", async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      "email": "test@example.com",
+      "password": "password" 
+    })
+    .expect(201);
+
+    expect(response.get("Set-Cookie")).toBeDefined();
+});
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,11 +1,9 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 
-import { app } from "../app";
-
 let mongo: any;
 beforeAll(async () => {
-process.env.JWT_KEY = "asdfghj";
+  process.env.JWT_KEY = "asdfghj";
 
   const mongo = await MongoMemoryServer.create();
   const mongiUri = mongo.getUri();
