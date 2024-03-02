d82c51d985efdef23a3681a30ae6bee27711c0ee
borosakos <boros.aki@gmail.com>
2024-03-02 21:05:05 +0100

    test: Add signin helper

--- auth/src/routes/__test__/currentUser.test.ts
+++ auth/src/routes/__test__/currentUser.test.ts
@@ -2,19 +2,22 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("responds with details about the current user", async () => {
-  const authResponse = await request(app)
-    .post("/api/users/signup")
-    .send({
-      "email": "test@example.com",
-      "password": "password"
-    })
-    .expect(201);
+  const cookie = await global.signin();
 
   const response = await request(app)
     .get("/api/users/currentuser")
-    .set("Cookie", authResponse.get("Set-Cookie"))
+    .set("Cookie", cookie)
     .send()
     .expect(200);
 
   expect(response.body.currentUser.email).toEqual("test@example.com");
+});
+
+it("responds with null if not authenticated", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toBeNull();
 });
