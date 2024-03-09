1e7a9da069bdfa438b7d8ae1f65b5e935a2fb65e
borosakos <boros.aki@gmail.com>
2024-03-09 20:33:17 +0100

    test [tickets]: Define requirements as tests

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,22 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("has a route handler listening to POST /api/tickets", async () => {
+
+});
+
+it("can only be access if the user is signed in", async () => {
+  
+});
+
+it("return an error if an invalid title is provided", async () => {
+  
+});
+
+it("returns an error if an invalid price is provided", async () => {
+  
+});
+
+it("creates a ticket with valid inputs", async () => {
+  
+});
