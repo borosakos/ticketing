00b4f6cc20cff4d67f3f6516b0ba8e9c45e82fa3
borosakos <boros.aki@gmail.com>
2024-03-13 00:24:16 +0100

    feat [common]: Define Listener base as generic

--- common/src/events/ticketCreatedListener.ts
+++ common/src/events/ticketCreatedListener.ts
@@ -1,13 +1,19 @@
 import { Message } from "node-nats-streaming"; 
 
 import { Listener } from "./baseListener";
+import { TicketCreatedEvent } from "./ticketCreatedEvent";
+import { Subjects } from "./subjects";
 
-class TicketCreatedListener extends Listener {
-  readonly subject = "ticket:created";
+class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
   readonly queueGroupName = "payments-service";
 
-  onMessage(data: any, msg: Message): void {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message): void {
     console.log("Event received!", data);
+    console.log(data.id);
+    console.log(data.title);
+    console.log(data.price);
+    
     msg.ack();
   }
 }
