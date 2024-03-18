da58528d5219de1d91e62f2758cd0cb906068665
borosakos <boros.aki@gmail.com>
2024-03-18 22:17:45 +0100

    test [orders]: Add input validation to createOrders

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import mongoose from "mongoose";
+
+import { requireAuth, validateRequest } from "@aboros-tickets/common";
 
 const router = express.Router();
 
-router.post("/api/orders", (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .notEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided!")
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as CreateOrderRouter };
