0ff12a6b28109eaa7d9c16429eafcbbc20d2fe42
borosakos <boros.aki@gmail.com>
2024-03-19 17:20:14 +0100

    feat [orders]: Add Create order business logic

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -2,9 +2,13 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import mongoose from "mongoose";
 
-import { requireAuth, validateRequest } from "@aboros-tickets/common";
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from "@aboros-tickets/common";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
   "/api/orders",
@@ -16,8 +20,33 @@ router.post(
       .withMessage("TicketId must be provided!")
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.send({});
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError("Ticket is already reserved!");
+    }
+
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket
+    });
+    await order.save();
+
+    // TODO: Publish Ticket Created event
+
+    res.status(201).send(order);
   }
 );
 
