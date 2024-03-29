796f73c596ca8a7fa5926fda1e25051f0e6b6706
borosakos <boros.aki@gmail.com>
2024-03-29 13:04:57 +0100

    feature [client]: Sanitize create ticket input

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -7,7 +7,9 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component currentUser={currentUser} {...pageProps} />
+      <div className="container">
+        <Component currentUser={currentUser} {...pageProps} />
+      </div>
     </div>
   );
 };
@@ -15,6 +17,7 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
 AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx);
   const { data } = await client.get("/api/users/currentuser");
+  console.log("curent user response: ", data)
 
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
