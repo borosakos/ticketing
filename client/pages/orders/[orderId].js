07d27ed54daa5113bce7c6de0aed3c62637353bd
borosakos <boros.aki@gmail.com>
2024-03-30 21:11:10 +0100

    feature [client]: Show user's orders

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import StripeCheckout from "react-stripe-checkout";
+import Router from "next/router";
+
 import useRequest from "../../hooks/useRequest";
 
 const OrderShow = ({ order, currentUser }) => {
@@ -10,7 +12,7 @@ const OrderShow = ({ order, currentUser }) => {
     body: {
       orderId: order.id
     },
-    onSuccess: (payment) => console.log(payment)
+    onSuccess: () => Router.push("/orders")
   });
   
   useEffect(() => {
